2b4dcd8bf4688095015c0f3dce719b3785a69bfb
のびすけ <rsksound@gmail.com>
2017-01-26 05:45:51 +0900

    dj_update

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 'use strict';
 
 const PORT = process.env.PORT || 3000;
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const index = require('./routes/index');
-const upload = require('./routes/fileupload');
+// const upload = require('./routes/fileupload');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', index);
-app.use('/upload', upload);
+// app.use('/upload', upload);
 
 app.listen(PORT);
 console.log(`Server running at ${PORT}`);
