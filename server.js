bf73828172d69fe69cb8b2ad419bea03d072c074
のびすけ <rsksound@gmail.com>
2017-01-25 22:47:29 +0900

    DJ: 調整

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const MilkCocoa = require('milkcocoa');
 const milkcocoa = new MilkCocoa(`${process.env.MC_ID}.mlkcca.com`);
 const ds = milkcocoa.dataStore('ytdata');
 
+
 const http = require('http');
 const https = require('https');
 const crypto = require('crypto');
